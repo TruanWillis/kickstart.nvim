e74155c9497299125be2ac05bd2c1f64a112371d
Truan Willis <truanwillis@pop-os.lan>
2025-03-11 21:54:04 +0200

    develop mini move and files added

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -31,7 +31,14 @@ return { -- Collection of various small independent plugins/modules
     --   return '%2l:%-2v'
     -- end
 
+    require('mini.files').setup()
+    require('mini.move').setup()
     -- ... and there is more!
     --  Check out: https://github.com/echasnovski/mini.nvim
   end,
+  keymaps = {
+    vim.keymap.set('n', '<leader>e', function()
+      MiniFiles.open()
+    end, { desc = 'File [E]xploer' }),
+  },
 }
