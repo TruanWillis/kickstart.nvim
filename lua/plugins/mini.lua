841cfd317a3630bb8c464a21dd4ae7d586d2b79e
truanwillis <truan@agiletek.co.uk>
2025-03-19 08:50:59 +0000

    develop added custom key maps to mini.surround needs further testing

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -14,7 +14,20 @@ return { -- Collection of various small independent plugins/modules
     -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
     -- - sd'   - [S]urround [D]elete [']quotes
     -- - sr)'  - [S]urround [R]eplace [)] [']
-    require('mini.surround').setup()
+    require('mini.surround').setup {
+      mappings = {
+        add = 'msa', -- Add surrounding in Normal and Visual modes
+        delete = 'msd', -- Delete surrounding
+        find = 'msf', -- Find surrounding (to the right)
+        find_left = 'msF', -- Find surrounding (to the left)
+        highlight = 'msh', -- Highlight surrounding
+        replace = 'msr', -- Replace surrounding
+        update_n_lines = 'msn', -- Update `n_lines`
+
+        suffix_last = 'l', -- Suffix to search with "prev" method
+        suffix_next = 'n', -- Suffix to search with "next" method
+      },
+    }
 
     -- -- Simple and easy statusline.
     -- --  You could remove this setup call if you don't like it,
