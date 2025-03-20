dcb6d6f8e71535d2e197ca59c16f7a6ee271f3f0
truanwillis <truan@agiletek.co.uk>
2025-03-20 15:32:25 +0000

    develop mini surround default keymaps assigned, flash keymaps suppressed

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -16,13 +16,13 @@ return { -- Collection of various small independent plugins/modules
     -- - sr)'  - [S]urround [R]eplace [)] [']
     require('mini.surround').setup {
       mappings = {
-        add = 'msa', -- Add surrounding in Normal and Visual modes
-        delete = 'msd', -- Delete surrounding
-        find = 'msf', -- Find surrounding (to the right)
-        find_left = 'msF', -- Find surrounding (to the left)
-        highlight = 'msh', -- Highlight surrounding
-        replace = 'msr', -- Replace surrounding
-        update_n_lines = 'msn', -- Update `n_lines`
+        add = 'sa', -- Add surrounding in Normal and Visual modes
+        delete = 'sd', -- Delete surrounding
+        find = 'sf', -- Find surrounding (to the right)
+        find_left = 'sF', -- Find surrounding (to the left)
+        highlight = 'sh', -- Highlight surrounding
+        replace = 'sr', -- Replace surrounding
+        update_n_lines = 'sn', -- Update `n_lines`
 
         suffix_last = 'l', -- Suffix to search with "prev" method
         suffix_next = 'n', -- Suffix to search with "next" method
