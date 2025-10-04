f1e16979b5b10b69c62f6af14d393203fb9d302f
truanwillis <truan@agiletek.co.uk>
2025-10-04 10:02:48 +0300

    develop autocommands search commented out, not sure why I'd use it

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -45,7 +45,7 @@ return {
     { "<leader>sD", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
     { "<leader>sh", function() Snacks.picker.help() end, desc = "Help Pages" },
     { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
-    { "<leader>sa", function() Snacks.picker.autocmds() end, desc = "Autocmds" },
+    -- { "<leader>sa", function() Snacks.picker.autocmds() end, desc = "Autocmds" },
     { "<leader>sc", function() Snacks.picker.command_history() end, desc = "Command History" },
     { "<leader>sC", function() Snacks.picker.commands() end, desc = "Commands" },
     { "<leader>sn", function() Snacks.picker.notifications() end, desc = "Notification History" },
