16e335ff50395520d6fcc0d3f64e654f0460d1f6
truanwillis <truan@agiletek.co.uk>
2025-10-16 18:31:00 +0100

    develop mini keybindings updated, not sure if I like them

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -15,33 +15,33 @@ return {
     -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
     -- - sd'   - [S]urround [D]elete [']quotes
     -- - sr)'  - [S]urround [R]eplace [)] [']
-    -- require('mini.surround').setup {
-    --   mappings = {
-    --     add = 'msa', -- Add surrounding in Normal and Visual modes
-    --     delete = 'msd', -- Delete surrounding
-    --     find = 'msf', -- Find surrounding (to the right)
-    --     find_left = 'msF', -- Find surrounding (to the left)
-    --     highlight = 'msh', -- Highlight surrounding
-    --     replace = 'msr', -- Replace surrounding
-    --     update_n_lines = 'msn', -- Update `n_lines`
-    --     suffix_last = 'l', -- Suffix to search with "prev" method
-    --     suffix_next = 'n', -- Suffix to search with "next" method
-    --   },
-    -- }
+    require('mini.surround').setup {
+      mappings = {
+        add = 'ysa', -- Add surrounding in Normal and Visual modes
+        delete = 'ysd', -- Delete surrounding
+        find = 'ysf', -- Find surrounding (to the right)
+        find_left = 'ysF', -- Find surrounding (to the left)
+        highlight = 'ysh', -- Highlight surrounding
+        replace = 'ysr', -- Replace surrounding
+        update_n_lines = 'ysn', -- Update `n_lines`
+        suffix_last = 'l', -- Suffix to search with "prev" method
+        suffix_next = 'n', -- Suffix to search with "next" method
+      },
+    }
     require('mini.move').setup()
     require('mini.pairs').setup()
 
     -- Register with which-key
     local wk = require('which-key')
     wk.add({
-      { "ms", group = "Mini surround" },
-      { "msa", desc = "Add surrounding", mode = { "n", "v" } },
-      { "msd", desc = "Delete surrounding" },
-      { "msf", desc = "Find surrounding (right)" },
-      { "msF", desc = "Find surrounding (left)" },
-      { "msh", desc = "Highlight surrounding" },
-      { "msr", desc = "Replace surrounding" },
-      { "msn", desc = "Update n_lines" },
+      { "ys", group = "Mini surround" },
+      { "ysa", desc = "Add surrounding", mode = { "n", "v" } },
+      { "ysd", desc = "Delete surrounding" },
+      { "ysf", desc = "Find surrounding (right)" },
+      { "ysF", desc = "Find surrounding (left)" },
+      { "ysh", desc = "Highlight surrounding" },
+      { "ysr", desc = "Replace surrounding" },
+      { "ysn", desc = "Update n_lines" },
     })
     -- don't init mini.files here (we’ll load it with keys below)
   end,
