75cda98eb7bc13657c32aa6f18bbc2fbc4872516
truanwillis <truan@agiletek.co.uk>
2025-11-18 08:57:41 +0000

    develop autocomplete and treesitter updated to try and improve startup time

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,8 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
+  event = "BufReadPre",
   build = ':TSUpdate',
+  cmd = "TSUpdate",
   main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   dependencies = {
     'nvim-treesitter/nvim-treesitter-textobjects', -- Add textobjects plugin
